da41c7a1a8e0cb3f16344c13580d995f75f10f52
Dan Kelly <dpk5e7@yahoo.com>
2022-11-02 21:02:00 -0600

    sassy projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,3 +1,54 @@
-export default function Project() {
-  return <h2 className="text-primary">Project</h2>;
+import svgGitHub from "../images/github.svg";
+
+export default function Project(props) {
+  const title = props.title;
+
+  const link = props.url ? (
+    <a href={props.url} target="_blank" rel="noreferrer">
+      {title}
+    </a>
+  ) : (
+    ""
+  );
+
+  const imgLink =
+    props.url && props.image ? (
+      <a href={props.url} target="_blank" rel="noreferrer">
+        <img src={props.image} alt={title} />
+      </a>
+    ) : (
+      ""
+    );
+
+  const description = props.description ? <p>{props.description}</p> : "";
+
+  const technologies = props.technologies ? (
+    <p>Technologies: {props.technologies}</p>
+  ) : (
+    ""
+  );
+
+  const GitHubRepoLink = props.GitHubRepo ? (
+    <p>
+      {
+        <a href={props.GitHubRepo} target="_blank" rel="noreferrer">
+          <img src={svgGitHub} alt="GitHub" />
+        </a>
+      }
+    </p>
+  ) : (
+    ""
+  );
+
+  return (
+    <article>
+      <header>
+        <h3>{link}</h3>
+        {GitHubRepoLink}
+      </header>
+      {imgLink}
+      {description}
+      {technologies}
+    </article>
+  );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,188 @@
+import Project from "../Project";
 import imgKSS from "../../images/code.jpg";
 import imgCollegeBowlGames from "../../images/FaurotField.jpg";
 import imgEvRoute from "../../images/evRoute.jpg";
 import imgPowderBound from "../../images/powder-bound.jpg";
+import imgSocialNetworkAPI from "../../images/social-network-api.jpg";
+import imgTechBlog from "../../images/tech-blog.jpg";
+import imgECommerceBackend from "../../images/e-commerce-back-end.jpg";
+import imgEmployeeTracker from "../../images/employee-tracker.jpg";
+import imgTeamProfileGenerator from "../../images/team-profile-generator.jpg";
+import imgWeatherDashboard from "../../images/weather-dashboard.jpg";
+import imgCodingQuiz from "../../images/coding-quiz.jpg";
+import imgJATE from "../../images/jate.jpg";
+import imgNoteTaker from "../../images/note-taker.jpg";
+import imgReadmeGenerator from "../../images/readme-generator.jpg";
+import imgWorkDayScheduler from "../../images/work-day-scheduler.jpg";
+import imgPasswordGenerator from "../../images/password-generator.jpg";
+import imgPortfolio from "../../images/portfolio.jpg";
+import imgAccessibilityRefactor from "../../images/accessibility-refactor.jpg";
+
+const projects = [
+  {
+    title: "Kelly Software Solutions",
+    url: "https://www.kellysoftware.org",
+    image: imgKSS,
+    description:
+      "A collection of coding projects in various languages and technologies.",
+    technologies: "ASP.NET, C#, Java",
+  },
+  {
+    title: "College Bowl Games",
+    url: "https://bowlgames.kellysoftware.org",
+    image: imgCollegeBowlGames,
+    description:
+      "A free Pick'em-style college football site. This site was originally written in 2007.",
+    technologies: "Microsoft ASP.NET, C#, & SQL Server",
+  },
+  {
+    title: "evRoute",
+    url: "https://dpk5e7-evroute.herokuapp.com/",
+    image: imgEvRoute,
+    description:
+      "Project #2 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge was to build an application to assist Electric Vehicle owners in planning long distance trips.",
+    technologies:
+      "Alternative Fuel Data Center information, Mapbox, Node.js, Express.js, MySQL, Sequelize, SASS, Handlebars.js, & express-session",
+    GitHubRepo: "https://github.com/dpk5e7/evRoute",
+  },
+  {
+    title: "Powder Bound",
+    url: "https://www.kellysoftware.org",
+    image: imgPowderBound,
+    description:
+      "Project #1 for the University of Denver Coding Bootcamp 2022. The purpose of this project was to build an application to assist snow sport enthusiasts in Colorado in planning their search for powder.",
+    technologies:
+      "Powderlin.es, Open Weather, Google Static Maps, MapQuest Directions, MapQuest GeoCoding & Reverse Geocoding, MapQuest Static Maps, MapQuest Search Ahead, Geolocation",
+    GitHubRepo: "https://github.com/dpk5e7/powder-bound",
+  },
+  {
+    title: "Just Another Text Editor",
+    url: "https://dpk5e7-jate.herokuapp.com/",
+    image: imgJATE,
+    description:
+      "Challenge #19 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to a text editor that runs in the browser. It is a single-page application that meets the progressive web application (PWA) criteria.",
+    technologies: "Node.js, Express.js, WebPack, WorkBox, & IndexedDB",
+    GitHubRepo: "https://github.com/dpk5e7/jate",
+  },
+  {
+    title: "Social Network API",
+    url: "https://github.com/dpk5e7/social-network-api",
+    image: imgSocialNetworkAPI,
+    description:
+      "Challenge #18 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to understand the unstructured nature of NoSQL by building an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list.",
+    technologies: "Node.js, Express.js, MongoDB, & Mongoose",
+    GitHubRepo: "https://github.com/dpk5e7/social-network-api",
+  },
+  {
+    title: "Tech Blog",
+    url: "https://dpk5e7-tech-blog.herokuapp.com/",
+    image: imgTechBlog,
+    description:
+      "Challenge #14 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to build a CMS-style blog site following the MVC paradigm, where developers can publish their blog posts and comment on other developers' posts as well.",
+    technologies:
+      "Node.js, Express.js, MySQL, Sequelize, Handlebars.js, & express-session",
+    GitHubRepo: "https://github.com/dpk5e7/tech-blog",
+  },
+  {
+    title: "e-Commerce Back End",
+    url: "https://github.com/dpk5e7/e-commerce-back-end",
+    image: imgECommerceBackend,
+    description:
+      "Challenge #13 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to build a back end for a generic e-commerce site using one-to-many and many-to-many database relationships.",
+    technologies: "Node.js, Express.js, MySQL, & Sequelize",
+    GitHubRepo: "https://github.com/dpk5e7/e-commerce-back-end",
+  },
+  {
+    title: "Employee Tracker",
+    url: "https://github.com/dpk5e7/employee-tracker",
+    image: imgEmployeeTracker,
+    description:
+      "Challenge #12 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to build a command-line application to manage a company's employee database.",
+    technologies: "Node.js, Inquirer v8.2.4, dotenv, & MySQL",
+    GitHubRepo: "https://github.com/dpk5e7/employee-tracker",
+  },
+  {
+    title: "Note Taker",
+    url: "https://dpk5e7-note-taker.herokuapp.com/",
+    image: imgNoteTaker,
+    description:
+      "Challenge #11 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to build a note taker application to save and retreive data from a JSON file.",
+    technologies: "Node.js, Express.js",
+    GitHubRepo: "https://github.com/dpk5e7/note-taker",
+  },
+  {
+    title: "Team Profile Generator",
+    url: "https://github.com/dpk5e7/team-profile-generator/",
+    image: imgTeamProfileGenerator,
+    description:
+      "Challenge #10 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to build a team profile generator and to use Jest for unit testing.",
+    technologies: "Node.js, Inquirer v8.2.4, and Jest v28.1.3",
+    GitHubRepo: "https://github.com/dpk5e7/team-profile-generator",
+  },
+  {
+    title: "Readme Generator",
+    url: "https://github.com/dpk5e7/readme-generator/",
+    image: imgReadmeGenerator,
+    description:
+      "Challenge #9 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to build a GitHub README generator.",
+    technologies: "Node.js & Inquirer v8.2.4",
+    GitHubRepo: "https://github.com/dpk5e7/readme-generator/",
+  },
+  {
+    title: "Weather Dashboard",
+    url: "https://dpk5e7.github.io/weather-dashboard/",
+    image: imgWeatherDashboard,
+    description:
+      "Challenge #6 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to build a weather dashboard that calls the OpenWeather API.",
+    technologies: "HTML, CSS, Moment.js, Bootstrap 5.2, & OpenWeather API",
+    GitHubRepo: "https://github.com/dpk5e7/weather-dashboard",
+  },
+  {
+    title: "Work Day Scheduler",
+    url: "https://dpk5e7.github.io/work-day-scheduler/",
+    image: imgWorkDayScheduler,
+    description:
+      "Challenge #5 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to build a work day scheduler.",
+    technologies: "HTML, CSS, jQuery, Moment.js & Bootstrap 5.2",
+    GitHubRepo: "https://github.com/dpk5e7/work-day-scheduler",
+  },
+  {
+    title: "Coding Quiz",
+    url: "https://dpk5e7.github.io/coding-quiz/",
+    image: imgCodingQuiz,
+    description:
+      "Challenge #4 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to build a timed, multiple choice coding assessment quiz.",
+    technologies: "HTML, CSS, & Javascript",
+    GitHubRepo: "https://github.com/dpk5e7/coding-quiz",
+  },
+  {
+    title: "Password Generator",
+    url: "https://dpk5e7.github.io/password-generator/",
+    image: imgPasswordGenerator,
+    description:
+      "Challenge #3 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected.",
+    technologies: "Javascript",
+    GitHubRepo: "https://github.com/dpk5e7/password-generator",
+  },
+  {
+    title: "Portfolio",
+    url: "ttps://dpk5e7.github.io/portfolio/",
+    image: imgPortfolio,
+    description:
+      "Challenge #2 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to build a portfolio.",
+    technologies: "HTML & CSS",
+    GitHubRepo: "https://github.com/dpk5e7/portfolio",
+  },
+  {
+    title: "Accessibility Refactor",
+    url: "https://dpk5e7.github.io/accessibility-refactor/",
+    image: imgAccessibilityRefactor,
+    description:
+      "Challenge #1 for the University of Denver Coding Bootcamp 2022. The purpose of this challenge is to refactor an existing site to make it more accessible.",
+    technologies: "HTML & CSS",
+    GitHubRepo: "https://github.com/dpk5e7/accessibility-refactor",
+  },
+];
 
 export default function Portfolio() {
   return (
@@ -9,122 +190,16 @@ export default function Portfolio() {
       <section id="Portfolio">
         <h2>Portfolio</h2>
         <div class="articles">
-          <article>
-            <header>
-              <h3>
-                <a
-                  href="https://www.kellysoftware.org"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Kelly Software Solutions
-                </a>
-              </h3>
-            </header>
-            <a
-              href="https://www.kellysoftware.org"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={imgKSS} alt="Code" />
-            </a>
-            <p>
-              A collection of coding projects in various languages and
-              technologies.
-            </p>
-          </article>
-
-          <article>
-            <header>
-              <h3>
-                <a
-                  href="https://bowlgames.kellysoftware.org"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  College Bowl Games
-                </a>
-              </h3>
-            </header>
-            <a
-              href="https://bowlgames.kellysoftware.org"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={imgCollegeBowlGames} alt="Football Game" />
-            </a>
-            <p>
-              A free Pick'em-style college football site. This site was
-              originally written in 2007.
-            </p>
-            <p>Microsoft ASP.NET, C#, & SQL Server</p>
-          </article>
-
-          <article>
-            <header>
-              <h3>
-                <a
-                  href="https://dpk5e7-evroute.herokuapp.com/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  evRoute
-                </a>
-              </h3>
-            </header>
-            <a
-              href="https://dpk5e7-evroute.herokuapp.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={imgEvRoute} alt="evRoute" />
-            </a>
-            <p>
-              Project #2 for the University of Denver Coding Bootcamp 2022. The
-              purpose of this challenge was to build an application to assist
-              Electric Vehicle owners in planning long distance trips.
-            </p>
-            <p>
-              Alternative Fuel Data Center information, Mapbox, Node.js,
-              Express.js, MySQL, Sequelize, SASS, Handlebars.js, &
-              express-session
-            </p>
-          </article>
-
-          <article>
-            <header>
-              <h3>
-                <a
-                  href="https://dpk5e7.github.io/powder-bound/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Powder Bound
-                </a>
-              </h3>
-            </header>
-            <a
-              href="https://dpk5e7.github.io/powder-bound/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={imgPowderBound} alt="Powder Bound" />
-            </a>
-            <p>
-              Project #1 for the University of Denver Coding Bootcamp 2022. The
-              purpose of this project was to build an application to assist snow
-              sport enthusiasts in Colorado in planning their search for powder.
-            </p>
-            <p>
-              HTML, CSS, Moment.js v2.27.0, jQuery v3.6.0, Bulma v0.9.4, &
-              Leaflet v1.8.0
-            </p>
-            <p>
-              Powderlin.es, Open Weather, Google Static Maps, MapQuest
-              Directions, MapQuest GeoCoding & Reverse Geocoding, MapQuest
-              Static Maps, MapQuest Search Ahead, Geolocation
-            </p>
-          </article>
+          {projects.map((project) => (
+            <Project
+              title={project.title}
+              url={project.url}
+              image={project.image}
+              description={project.description}
+              technologies={project.technologies}
+              GitHubRepo={project.GitHubRepo}
+            />
+          ))}
         </div>
       </section>
     </div>
