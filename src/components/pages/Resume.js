4a1db6ab9108009cb1fd4d988e1ca62ce1129efe
Dan Kelly <dpk5e7@yahoo.com>
2022-11-03 19:53:22 -0600

    resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,32 @@
 export default function Resume() {
+  
+  const resumeLink = <a href={process.env.PUBLIC_URL + "/downloads/Kelly_Resume_2022.pdf"} alt="Resume" download>resume</a>;
+
   return (
     <div class="sections">
       <section id="Contact">
         <h2>Resume</h2>
-        <p>WHEN I am presented with the Resume section</p>
+        <p>Download my {resumeLink}.</p>
         <p>
-          THEN I see a link to a downloadable resume and a list of the
-          developer’s proficiencies
+          <h3>Front-End Proficiencies:</h3>
+          <ul>
+            <li>HTML, CSS, JavaScript</li>
+            <li>React</li>
+            <li>Leaflet, MapBox</li>
+            <li>Bootstrap, SASS</li>
+            <li>RESTful APIs</li>
+          </ul>
+        </p>
+        <p>
+          <h3>Back-End Proficiencies:</h3>
+          <ul>
+            <li>C#</li>
+            <li>ASP.NET</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>MS SQL, MySQL, MongoDB</li>
+            <li>Sequelize, Mongoose</li>
+          </ul>
         </p>
       </section>
     </div>
