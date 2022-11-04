bad4e70e4d662cbbddfd1aa69fd26796d5c03221
Dan Kelly <dpk5e7@yahoo.com>
2022-11-04 17:57:39 -0600

    pwa work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "./scss/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,3 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
